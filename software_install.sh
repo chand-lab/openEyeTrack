efc95753347e503032ce986fdbccb191f102ce14
jpcasas <jpaolocasas@gmail.com>
2019-07-12 15:05:01 -0400

    added installation script

---
+++ software_install.sh
@@ -0,0 +1,47 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+#install git
+sudo apt-get install git-core
+
+#install qt5 first
+sudo apt-get install build-essential
+sudo apt-get install qt5-default
+
+#install opencv4
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y remove x264 libx264-dev
+sudo apt -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt -y install git gfortran
+sudo apt -y install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt -y install libtiff5-dev
+sudo apt -y install libtiff-dev
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt -y install libxine2-dev libv4l-dev
+sudo apt -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt -y install libatlas-base-dev
+sudo apt -y install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt -y install libvorbis-dev libxvidcore-dev
+sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt -y install libavresample-dev
+sudo apt -y install x264 v4l-utils
+sudo apt -y install libprotobuf-dev protobuf-compiler
+sudo apt -y install libgoogle-glog-dev libgflags-dev
+sudo apt -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 4.1.0
+cd ..
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 4.1.0
+cd ..
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -D BUILD_EXAMPLES=OFF -D OPENCV_GENERATE_PKGCONFIG=ON ..
+sudo make install -j15
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
